a68748956f0c8983595ec2593a07685c68b04e80
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2025-04-12 00:33:32 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express"; // Imports Express.js
+import reminderRoutes from "./routes/reminderRoutes.js";
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware.js';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/reminders", reminderRoutes);
+
+app.use(errorHandlerMiddleware);
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+}); // Starts the server on port 3000 and logs a confirmation message.
